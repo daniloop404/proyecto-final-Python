79210fa89cc4aaaa222ec6243d21b61b3483318f
daniloop404 <daniloop404@gmail.com>
2024-01-06 14:57:52 -0500

    first commit

---
+++ core/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'core/home.html')
+
+def about(request):
+    return render(request, 'core/about.html')
+
+def contact(request):
+    return render(request, 'core/contact.html')
+
+def product(request):
+    return render(request, 'core/product.html')
+
+def service(request):
+    return render(request, 'core/service.html')
+
+def team(request):
+    return render(request, 'core/team.html')
+
+def testimonial(request):
+    return render(request, 'core/testimonial.html')
+
+def notfound(request):
+    return render(request, 'core/404.html')
