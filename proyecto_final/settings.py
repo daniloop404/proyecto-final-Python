62cd79fa6b88f371b58b9fadf2d4933906cae298
daniloop404 <daniloop404@gmail.com>
2024-01-17 19:33:24 -0500

    agregado social

--- proyecto_final/settings.py
+++ proyecto_final/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "products",
     "testimonial",
     "about",
+    "social",
     'ckeditor',
 ]
 
@@ -69,6 +70,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "social.processors.ctx_dict"
             ],
         },
     },
---
+++ social/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class Social(models.Model):
+    key=models.SlugField(max_length=50, unique=True, verbose_name="Clave")
+    name=models.CharField(max_length=50, verbose_name="Red social")
+    url=models.URLField(null=True, blank=True, verbose_name="Enlace")
+    created=models.DateTimeField(auto_now=True, verbose_name="Fecha de edicion")
+    updated=models.DateTimeField(auto_now=True, verbose_name="Fecha de edicion")
+
+    class Meta:
+        verbose_name="enlace"
+        verbose_name_plural="enlaces"
+        ordering=['name']
+
+    def __str__(self):
+        return self.name
+        
